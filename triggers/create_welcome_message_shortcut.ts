690ac8021e594470366e09e459784b265ce34d21
Neil Shapiro <nshapiro91@gmail.com>
2023-05-10 08:15:49 -0700

    Use TriggerContextData and disable lock file (#24)

--- triggers/create_welcome_message_shortcut.ts
+++ triggers/create_welcome_message_shortcut.ts
@@ -1,20 +1,21 @@
 import { Trigger } from "deno-slack-api/types.ts";
 import MessageSetupWorkflow from "../workflows/create_welcome_message.ts";
+import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
 
 /**
  * This link trigger prompts the MessageSetupWorkflow workflow.
  */
 const welcomeMessageTrigger: Trigger<typeof MessageSetupWorkflow.definition> = {
-  type: "shortcut",
+  type: TriggerTypes.Shortcut,
   name: "Setup a Welcome Message",
   description: "Creates an automated welcome message for a given channel.",
   workflow: `#/workflows/${MessageSetupWorkflow.definition.callback_id}`,
   inputs: {
     interactivity: {
-      value: "{{data.interactivity}}",
+      value: TriggerContextData.Shortcut.interactivity,
     },
     channel: {
-      value: "{{data.channel_id}}",
+      value: TriggerContextData.Shortcut.channel_id,
     },
   },
 };
